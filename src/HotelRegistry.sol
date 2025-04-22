3af60c3b32736dab1fe8fe8e850097efeabe9234
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-04-22 22:04:49 +0700

    fix wrong pragma ver

--- src/HotelRegistry.sol
+++ src/HotelRegistry.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
