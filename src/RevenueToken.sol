3695b038521d033a91ac9df40f288f0bfbcfa5ff
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-04-22 21:28:58 +0700

    feat remix friendly

--- src/RevenueToken.sol
+++ src/RevenueToken.sol
@@ -5,11 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RevenueToken is ERC20, Ownable {
-    constructor(string memory name_, string memory symbol_)
-        ERC20(name_, symbol_)
+    constructor(string memory name, string memory symbol)
+        ERC20(name, symbol)
         Ownable(msg.sender)
     {}
 
+    // Mint function that can only be called by the owner
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
